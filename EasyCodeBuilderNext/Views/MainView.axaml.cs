4fb00767070f5bb44073c09a6e137135d648601c
actbit <57023457+actbit@users.noreply.github.com>
2026-03-06 21:02:53 +0900

    [fix] views

--- EasyCodeBuilderNext/Views/MainView.axaml.cs
+++ EasyCodeBuilderNext/Views/MainView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using EasyCodeBuilderNext.Core.PluginSystem;
+using EasyCodeBuilderNext.ViewModels;
 
 namespace EasyCodeBuilderNext.Views;
 
@@ -7,5 +11,16 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void OnBlockTapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is Border border && border.Tag is BlockTemplate template)
+        {
+            if (DataContext is MainViewModel vm)
+            {
+                vm.AddBlockCommand.Execute(template);
+            }
+        }
     }
 }
